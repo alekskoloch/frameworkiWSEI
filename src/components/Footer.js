5b68c988eec07e3cf04ffbfdb14cf61064416535
Aleks Koloch <aleks.koloch@gmail.com>
2024-11-02 10:58:02 +0100

    Update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="text-center mt-4 py-4 bg-light">
-      <p>Autor: twoj_email@domena.pl</p>
+      <p>Autor: alekskoloch@wsei.pl</p>
     </footer>
   );
 };
